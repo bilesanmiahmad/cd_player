460004b088efe32f315e0ba5ba33c0ec6762747a
Bilesanmi Ahmad <sandraoamriagyeiwaa@gmail.com>
2018-08-07 10:56:59 +0000

    templates and all

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cd_library'
+    'cd_library',
+    'pastebin',
 ]
 
 MIDDLEWARE = [
---
+++ pastebin/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from pastebin.views import PasteCreate, PasteDetail, PasteList, PasteUpdate, PasteDelete
+
+urlpatterns = [
+    path('', PasteCreate.as_view(), name='create'),
+    path('paste/<int:pk>', PasteDetail.as_view(), name='pastebin_paste_detail'),
+    path('pastes/', PasteList.as_view(), name='pastebin_paste_list'),
+    path('paste/<int:pk>/edit', PasteUpdate, name='pastebin_paste_update'),
+    path('paste/<int:pk>/delete', PasteDelete, name='pastebin_paste_delete')
+]
